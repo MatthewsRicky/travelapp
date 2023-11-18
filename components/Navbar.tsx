0b271bf20dc62719b17df3465a9c3e9831b547b3
RickyMatthews <matthewsrickypro@gmail.com>
2023-11-18 22:12:26 +0300

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,10 +1,18 @@
+import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
 const Navbar = () => {
 	return (
-		<nav className='border-2 border-red-500 flex justify-between max-container padding-container relative z-30 py-5'>
-			<Link href='/'></Link>
+		<nav className=' flex justify-between max-container padding-container relative z-30 py-5'>
+			<Link href='/'>
+				<Image
+					src='/hilink-logo.svg'
+					alt='logo'
+					width={74}
+					height={29}
+				/>
+			</Link>
 		</nav>
 	);
 };
