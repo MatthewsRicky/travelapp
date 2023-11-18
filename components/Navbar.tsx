80024618f4fca638e6acc9e48bfee0f33d608444
RickyMatthews <matthewsrickypro@gmail.com>
2023-11-18 22:05:57 +0300

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,11 @@
+import Link from "next/link";
 import React from "react";
 
 const Navbar = () => {
 	return (
-		<nav className='border-2 border-red-500 flex justify-between max-container padding-container relative z-30 py-5'></nav>
+		<nav className='border-2 border-red-500 flex justify-between max-container padding-container relative z-30 py-5'>
+			<Link href='/'></Link>
+		</nav>
 	);
 };
 
