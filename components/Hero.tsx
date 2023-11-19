d337d15e1854fb36a7fabbe8bf84446f64319813
RickyMatthews <matthewsrickypro@gmail.com>
2023-11-19 08:44:28 +0300

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,8 +1,17 @@
-import React from "react";
-
+import Image from "next/image";
 const Hero = () => {
 	return (
-		<section className='max-container padding-container flex flex-col gap-20 py-10 pb-32 md:gap-28 lg:py-20 xl:flex-row border-2 border-red-500'></section>
+		<section className='max-container padding-container flex flex-col gap-20 py-10 pb-32 md:gap-28 lg:py-20 xl:flex-row border-2 border-red-500'>
+			<div className='hero-map'>{/* LEFT */}</div>
+			<div className='relative z-20 flex flex-col flex-1 xl:w-1/2'>
+				<Image
+					src='/camp.svg'
+					alt='camp'
+					width={50}
+					height={50}
+				/>
+			</div>
+		</section>
 	);
 };
 
