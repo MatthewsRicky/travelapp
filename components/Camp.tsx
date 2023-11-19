bb2d6179c65ab54f0e3d0291132e39e53f5d0b0a
RickyMatthews <matthewsrickypro@gmail.com>
2023-11-19 20:12:50 +0300

    Update Camp.tsx

--- components/Camp.tsx
+++ components/Camp.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 interface CampSiteProps {
 	backgroundImage: string;
 	title: string;
@@ -14,7 +16,18 @@ const CampSite = ({
 	return (
 		<div
 			className={`h-full w-full min-w-[100px] ${backgroundImage} bg-cover bg-no-repeat lg:rounded-r-5xl 2xl:rounded-5xl`}>
-			Camp sit 1
+			<div className='flex h-full flex-col items-start justify-between p-6 lg:px-20 lg:py-10'>
+				<div className='flexCenter gap-4'>
+					<div className='rounded-full bg-green-50 p-4'>
+						<Image
+							src='/folded-map.svg'
+							alt='map'
+							width={28}
+							height={28}
+						/>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 };
