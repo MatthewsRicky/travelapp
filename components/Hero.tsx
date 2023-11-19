505cec540196eefee317f1974337f71135820ebb
RickyMatthews <matthewsrickypro@gmail.com>
2023-11-19 15:26:51 +0300

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,7 +1,9 @@
+"use client";
+
 import Image from "next/image";
 const Hero = () => {
 	return (
-		<section className='max-container padding-container flex flex-col gap-20 py-10 pb-32 md:gap-28 lg:py-20 xl:flex-row border-2 border-red-500'>
+		<section className='max-container padding-container flex flex-col gap-20 py-10 pb-32 md:gap-28 lg:py-20 xl:flex-row'>
 			<div className='hero-map'>{/* LEFT */}</div>
 			<div className='relative z-20 flex flex-col flex-1 xl:w-1/2'>
 				<Image
@@ -34,6 +36,12 @@ const Hero = () => {
 								/>
 							))}
 					</div>
+					<p className='bold-16 lg:bold-20 text-blue-70'>
+						198k
+						<span className='regular-16 lg:regular-20 ml-1 underline'>
+							Excellent Reviews
+						</span>
+					</p>
 				</div>
 			</div>
 		</section>
