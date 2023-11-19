e58680ca7581a2d2265c19befa3554aeeb5c5c2a
RickyMatthews <matthewsrickypro@gmail.com>
2023-11-19 20:07:05 +0300

    Update Camp.tsx

--- components/Camp.tsx
+++ components/Camp.tsx
@@ -1,7 +1,35 @@
+interface CampSiteProps {
+	backgroundImage: string;
+	title: string;
+	subtitle: string;
+	peopleJoined: string;
+}
+
+const CampSite = ({
+	backgroundImage,
+	title,
+	subtitle,
+	peopleJoined,
+}: CampSiteProps) => {
+	return (
+		<div
+			className={`h-full w-full min-w-[100px] ${backgroundImage} bg-cover bg-no-repeat lg:rounded-r-5xl 2xl:rounded-5xl`}>
+			Camp sit 1
+		</div>
+	);
+};
+
 const Camp = () => {
 	return (
 		<section className='border-2 border-green-500 2xl:max-container relative flex flex-col py-10 lg:py-20 xl:mb-20'>
-			<div className='hide-scrollbar flex h-[340px] w-full items-start justify-start gap-8 overflow-x-auto lg:h-[400px] xl:h-[640px]'></div>
+			<div className='hide-scrollbar flex h-[340px] w-full items-start justify-start gap-8 overflow-x-auto lg:h-[400px] xl:h-[640px]'>
+				<CampSite
+					backgroundImage='bg-bg-img-1'
+					title='Camp Site'
+					subtitle='CHillSpot'
+					peopleJoined='50+ joined'
+				/>
+			</div>
 		</section>
 	);
 };
