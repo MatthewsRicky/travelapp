f10aa6bcb77c58ae8d24a5c49537fb2daa8c3fdc
RickyMatthews <matthewsrickypro@gmail.com>
2023-11-20 20:47:28 +0300

    Update Guide.tsx

--- components/Guide.tsx
+++ components/Guide.tsx
@@ -29,7 +29,7 @@ const Guide = () => {
 				</div>
 			</div>
 
-			<div className='flexCenter max-container relative w-full border-2 border-blue-700'>
+			<div className='flexCenter max-container relative w-full'>
 				<Image
 					src='/boat.png'
 					alt='boat'
@@ -38,6 +38,13 @@ const Guide = () => {
 					className='w-full object-center 2xl:rounded-5xl'
 				/>
 				<div className='absolute flex bg-white py-8 pl-5 pr-7 gap-3 rounded-3xl border shadow-md md:left-[5%] lg:top-20'>
+					<Image
+						src='/meter.svg'
+						alt='meter'
+						width={16}
+						height={158}
+						className='h-full w-auto'
+					/>
 					<div className='flexBetween flex-col'>
 						<div className='flex flex-col w-full'>
 							<div className='flexBetween gap-3 w-full'>
@@ -45,6 +52,13 @@ const Guide = () => {
 								<p className='bold-16 text-green-50'>48 mins</p>
 							</div>
 							<p className='bold-20 mt-2'>Kongo River</p>
+						</div>
+						<div className='flex flex-col w-full'>
+							<p className='regular-16 text-gray-20'>Start Track</p>
+
+							<h4 className='bold-20 mt-2 whitespace-nowrap'>
+								Galu Beach Diani
+							</h4>
 						</div>
 					</div>
 				</div>
