31d3b3b4519efc49850c23c3b157679a0e2943c1
RickyMatthews <matthewsrickypro@gmail.com>
2023-11-20 20:33:37 +0300

    Update Camp.tsx

--- components/Camp.tsx
+++ components/Camp.tsx
@@ -16,7 +16,7 @@ const CampSite = ({
 }: CampSiteProps) => {
 	return (
 		<div
-			className={`h-full w-full min-w-[100px] ${backgroundImage} bg-cover bg-no-repeat lg:rounded-r-5xl 2xl:rounded-5xl`}>
+			className={`h-full w-full min-w-[100px] ${backgroundImage} bg-cover bg-no-repeat lg:rounded-r-5xl 2xl:rounded-5xl shadow-xl`}>
 			<div className='flex h-full flex-col items-start justify-between p-6 lg:px-20 lg:py-10'>
 				<div className='flexCenter gap-4'>
 					<div className='rounded-full bg-green-50 p-4'>
@@ -54,7 +54,7 @@ const CampSite = ({
 const Camp = () => {
 	return (
 		<section className='2xl:max-container relative flex flex-col py-10 lg:py-20 xl:mb-20'>
-			<div className='hide-scrollbar flex h-[340px] w-full items-start justify-start gap-8 overflow-x-auto lg:h-[400px] xl:h-[640px] shadow-lg'>
+			<div className='hide-scrollbar flex h-[340px] w-full items-start justify-start gap-8 overflow-x-auto lg:h-[400px] xl:h-[640px]'>
 				<CampSite
 					backgroundImage='bg-bg-img-1'
 					title='Camp Site'
