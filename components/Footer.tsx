4fce393282030eba657b7573cc0c80dad308fd60
RickyMatthews <matthewsrickypro@gmail.com>
2023-11-21 13:54:05 +0300

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,4 +1,4 @@
-import { FOOTER_LINKS } from "@/constants";
+import { FOOTER_CONTACT_INFO, FOOTER_LINKS } from "@/constants";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
@@ -20,8 +20,33 @@ const Footer = () => {
 					</Link>
 					<div className='flex flex-wrap gap-10 sm:justify-between md:flex-1'>
 						{FOOTER_LINKS.map((columns) => (
-							<FooterColumn title={columns.title}>Footer</FooterColumn>
+							<FooterColumn title={columns.title}>
+								<ul className='regular-14 flex flex-col gap-4 text-gray-30'>
+									{columns.links.map((link) => (
+										<Link
+											href='/'
+											key={link}>
+											{link}
+										</Link>
+									))}
+								</ul>
+							</FooterColumn>
 						))}
+						<div className='flex flex-col gap-5'>
+							<FooterColumn title={FOOTER_CONTACT_INFO.title}>
+								{FOOTER_CONTACT_INFO.links.map((link) => (
+									<Link
+										href='/'
+										key={link.label}
+										className='flex gap-4 md:flex-col lg:flex-row'>
+										<p className='whitespace-nowrap'>{link.label}:</p>
+										<p className='whitespace-nowrap medium-14 text-blue-70'>
+											{link.value}:
+										</p>
+									</Link>
+								))}
+							</FooterColumn>
+						</div>
 					</div>
 				</div>
 			</div>
@@ -36,8 +61,8 @@ type FooterColumnProps = {
 
 const FooterColumn = ({ title, children }: FooterColumnProps) => {
 	return (
-		<div>
-			<h4>{title}</h4>
+		<div className='flex flex-col gap-5'>
+			<h4 className='bold-18 whitespace-nowrap'>{title}</h4>
 			{children}
 		</div>
 	);
