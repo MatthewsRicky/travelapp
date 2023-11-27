99d76533dad47baa0880563c9704c0fad733bce6
RickyMatthews <matthewsrickypro@gmail.com>
2023-11-27 18:13:50 +0300

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -4,65 +4,45 @@ import Image from "next/image";
 import Button from "./Button";
 const Hero = () => {
 	return (
-		<section className='max-container padding-container flex flex-col gap-20 py-10 pb-32 md:gap-28 lg:py-20 xl:flex-row'>
-			<div className='hero-map'>{/* LEFT */}</div>
-			<div className='relative z-20 flex flex-col flex-1 xl:w-1/2'>
+		<section className='max-container padding-container flex md:flex-row flex-col gap-20 py-10 pb-32 md:gap-28 lg:py-20 xl:flex-row'>
+			<div className=''>
 				<Image
-					src='/camp.svg'
+					src='/libosso_profile.webp'
 					alt='camp'
-					width={50}
-					height={50}
-					className='absolute left-[-5px] top-[-30px] w-10 lg:w-[50px]'
+					width={50o}
+					height={500}
+					className='w-10 lg:w-[50px]'
 				/>
-				<h1 className='bold-52 lg:bold-88'>Diani Beach Podcast</h1>
-				<p className='regular-32 mt-6 text-gray-30 xl:max-w-[520px]'>
-					Diani Beach Podcast is the ultra-shareable, uber-cool online guide to
-					things to do in Diani Beach.
-				</p>
-
-				<div className='my-11 flex flex-wrap gap-5'>
-					<div className='flex items-center gap-2'>
-						{Array(5)
-							.fill(1)
-							.map((_, index) => (
-								<Image
-									src='/star.svg'
-									alt='star'
-									width={24}
-									height={24}
-								/>
-							))}
-					</div>
-					<p className='bold-16 lg:bold-20 text-blue-70'>
-						198k
-						<span className='regular-16 lg:regular-20 ml-1 underline'>
-							Excellent Reviews
-						</span>
-					</p>
-				</div>
 			</div>
-			<div className='relative flex flex-1 items-start'>
-				<div className='relative flex z-20 w-[268px] flex-col gap-8 rounded-3xl bg-green-90 px-7 py-8 shadow-2xl'>
-					<div className='flex flex-col'>
-						<div className='flexBetween'>
-							<p className='regular-16 text-gray-20'>Location</p>
-							<Image
-								src='/close.svg'
-								alt='close'
-								width={24}
-								height={24}
-							/>
-						</div>
-						<p className='bold-20 text-white'>Tribearth</p>
-					</div>
-					<div className='flexBetween'>
-						<div className='flex flex-col'>
-							<p className='regular-16 block text-gray-20'>Distance</p>
-							<p className='bold-20 text-white'>173.28 mi</p>
-						</div>
-						<div className='flex flex-col'>
-							<p className='regular-16 block text-gray-20'>Elevation</p>
-							<p className='bold-20 text-white'>2.040 km</p>
+			<div className='relative z-20 flex flex-col flex-1 xl:w-1/2'>
+				<h1 className='bold-52 lg:bold-88'>Diani Beach Podcast</h1>
+
+				<div className='relative flex flex-1 items-start'>
+					<div className='relative flex z-20 w-[268px] flex-col gap-8 rounded-3xl bg-green-90 px-7 py-8 shadow-2xl'>
+						<p className='bold-32 mt-6 text-gray-30 xl:max-w-[520px]'>
+							Diani Beach Podcast is the ultra-shareable, uber-cool online guide
+							to things to do in Diani Beach.
+						</p>
+
+						<div className='my-11 flex flex-wrap gap-5'>
+							<div className='flex items-center gap-2'>
+								{Array(5)
+									.fill(1)
+									.map((_, index) => (
+										<Image
+											src='/star.svg'
+											alt='star'
+											width={24}
+											height={24}
+										/>
+									))}
+							</div>
+							<p className='bold-16 lg:bold-20 text-blue-70'>
+								198k
+								<span className='regular-16 lg:regular-20 ml-1 underline'>
+									Excellent Reviews
+								</span>
+							</p>
 						</div>
 					</div>
 				</div>
