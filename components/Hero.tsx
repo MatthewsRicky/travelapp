d338a583c6cf603ae1f8d6e6451a8541998574a5
RickyMatthews <matthewsrickypro@gmail.com>
2023-11-28 15:15:16 +0300

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 import Button from "./Button";
 const Hero = () => {
 	return (
-		<section className='max-container video-background padding-container flex md:flex-row flex-col gap-20 py-10 pb-32 md:gap-28 lg:py-20 xl:flex-row'>
+		<section className='max-container video-background padding-container flex flex-col gap-20 py-10 pb-32 md:gap-28 lg:py-20 xl:flex-row'>
 			<video
 				autoPlay
 				loop
@@ -14,18 +14,18 @@ const Hero = () => {
 					type='video/mp4'
 				/>
 			</video>
-			<div className='w-[50vw]'>
-				<Image
-					src='/libosso_profile.webp'
-					alt='camp'
-					width={500}
-					height={500}
-					className='shadow'
-				/>
-			</div>
-			<div className='relative z-20 flex flex-col flex-1 xl:w-1/2'>
-				<h1 className='bold-52 lg:bold-88'>Diani Beach Podcast</h1>
+			<h1 className='relative z-20 bold-52 lg:bold-88'>Diani Beach Podcast</h1>
 
+			<div className='relative z-20 flex flex-1 xl:w-1/2'>
+				<div className='relative z-20 w-[50vw]'>
+					<Image
+						src='/libosso_profile.webp'
+						alt='camp'
+						width={500}
+						height={500}
+						className='shadow'
+					/>
+				</div>
 				<div className='relative flex flex-1 items-start'>
 					<div className='relative flex z-20 w-[268px] flex-col gap-8 rounded-3xl bg-green-90 px-7 py-8 shadow-2xl'>
 						<p className='bold-32 mt-6 text-gray-30 xl:max-w-[520px]'>
