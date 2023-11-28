7fdf110c9721a8df273ce623e4cccf8251f3fd0b
RickyMatthews <matthewsrickypro@gmail.com>
2023-11-28 19:48:48 +0300

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 import Button from "./Button";
 const Hero = () => {
 	return (
-		<section className='max-container video-background padding-container flex flex-col gap-20 py-10 pb-32 md:gap-28 lg:py-20 xl:flex-row'>
+		<section className='max-container video-background padding-container flex flex-col gap-20 py-10 pb-32 md:gap-28 lg:py-20'>
 			<video
 				autoPlay
 				loop
@@ -14,21 +14,26 @@ const Hero = () => {
 					type='video/mp4'
 				/>
 			</video>
-			<h1 className='relative z-20 bold-52 lg:bold-88'>Diani Beach Podcast</h1>
+			<h1 className='relative z-20 bold-52 lg:bold-88 lg:w-[50%] lg:mx-auto px-4 py-6 rounded-md mt-6 text-center bg-blue-700/70 text-blue-200 shadow-lg'>
+				Diani Beach Podcast
+			</h1>
 
 			<div className='relative z-20 flex flex-1 xl:w-1/2'>
-				<div className='relative z-20 w-[50vw]'>
+				<div className='relative flex flex-col z-20 w-[50vw]'>
 					<Image
 						src='/libosso_profile.webp'
-						alt='camp'
-						width={500}
-						height={500}
-						className='shadow'
+						alt='Libosso'
+						width={300}
+						height={300}
+						className='shadow-lg rounded-full'
 					/>
+					<span>
+						<p className='bold-32'>Contributor: Vincent Embukane Liboso</p>
+					</span>
 				</div>
 				<div className='relative flex flex-1 items-start'>
-					<div className='relative flex z-20 w-[268px] flex-col gap-8 rounded-3xl bg-green-90 px-7 py-8 shadow-2xl'>
-						<p className='bold-32 mt-6 text-gray-30 xl:max-w-[520px]'>
+					<div className='relative flex z-20 w-[268px] flex-col gap-8  px-7 py-8 shadow-2xl'>
+						<p className='bold-32 px-4 py-6 rounded-md mt-6 text-center bg-blue-700/70 text-blue-200 shadow-lg xl:max-w-[520px]'>
 							Diani Beach Podcast is the ultra-shareable, uber-cool online guide
 							to things to do in Diani Beach.
 						</p>
