3677964eacd762364d216b7aa5a17b785bf124da
RickyMatthews <matthewsrickypro@gmail.com>
2023-11-28 11:18:41 +0300

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,11 +1,11 @@
 "use client";
 
 import Image from "next/image";
-import Button from "./Button";
+
 const Hero = () => {
 	return (
 		<section className='max-container padding-container flex md:flex-row flex-col gap-20 py-10 pb-32 md:gap-28 lg:py-20 xl:flex-row'>
-			<div className=''>
+			<div className='w-[50vw]'>
 				<Image
 					src='/libosso_profile.webp'
 					alt='camp'
