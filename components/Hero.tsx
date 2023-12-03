3dfd8ec4616ab7835b53a188e279d5315c8ea823
RickyMatthews <matthewsrickypro@gmail.com>
2023-12-03 15:51:21 +0300

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 import Button from "./Button";
 const Hero = () => {
 	return (
-		<section className='max-container video-background padding-container flex flex-col gap-8 py-10 pb-32 md:gap-28 lg:py-20'>
+		<section className='max-container video-background padding-container flex flex-col gap-8 py-10'>
 			<video
 				autoPlay
 				loop
@@ -14,7 +14,7 @@ const Hero = () => {
 					type='video/mp4'
 				/>
 			</video>
-			<h1 className='w-[30%] flex justify-center items-center relative z-20 bold-32 lg:bold-52 lg:w-[30%] lg:mx-auto my-4 px-4 py-6 rounded-md mt-6 text-center bg-blue-700/70 text-blue-200 shadow-lg'>
+			<h1 className='w-[30%] mx-auto flex justify-center items-center relative z-20 bold-32 lg:bold-52 lg:w-[30%] lg:mx-auto my-4 px-4 py-6 rounded-md mt-2 text-center bg-blue-700/70 text-blue-200 shadow-lg'>
 				Diani Beach Podcast
 			</h1>
 
@@ -27,16 +27,16 @@ const Hero = () => {
 								alt='Libosso'
 								width={250}
 								height={250}
-								className=''
+								className='rounded-md drop-shadow-lg shadow-blue-950'
 							/>
 						</div>
-						<div className='flex flex-col w-[40%]'>
-							<p className='bold-24 px-4 py-6 rounded-md mt-6 text-center bg-blue-700/70 text-blue-200 shadow-lg xl:max-w-[520px]'>
+						<div className='flex flex-col gap-4 w-[40%]'>
+							<p className='bold-24 px-4 py-6 rounded-md mt-6 text-center bg-blue-700/80 text-blue-200 shadow-lg xl:max-w-[520px]'>
 								Diani Beach Podcast is the ultra-shareable, uber-cool online
 								guide to things to do in Diani Beach.
 							</p>
-							<p className='text-lg font-semibold text-blue-800'>
-								Contributor: Vincent Embukane Liboso
+							<p className='text-md font-semibold p-3 shadow-lg text-blue-900 rounded-md bg-blue-300/50 text-center'>
+								Vincent Embukane Liboso
 							</p>
 						</div>
 					</div>
