5378577ce4cd0a4f3ed70d4d1214b91f0ca5b693
RickyMatthews <matthewsrickypro@gmail.com>
2023-12-03 15:58:50 +0300

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -20,7 +20,7 @@ const Hero = () => {
 
 			<div className='relative z-20 flex flex-shrink gap-6 flex-col sm:flex-row items-center mb-20 justify-around'>
 				<div className='relative flex flex-col'>
-					<div className='justify-between items-center relative bg-blue-100/10 flex z-20 w-full fle flex-row gap-8 px-7 py-3 shadow-2xl'>
+					<div className='justify-around items-center relative mx-auto bg-blue-100/10 flex z-20 w-full sm:w-[85%] flex-col sm:flex-row gap-4 px-7 py-3 shadow-2xl'>
 						<div className='relative flex flex-col z-20 shadow-lg w-[50%] rounded-lg'>
 							<Image
 								src='/libosso_profile.webp'
@@ -31,11 +31,11 @@ const Hero = () => {
 							/>
 						</div>
 						<div className='flex flex-col gap-4 w-[40%]'>
-							<p className='bold-24 px-4 py-6 rounded-md mt-6 text-center bg-blue-700/80 text-blue-200 shadow-lg xl:max-w-[520px]'>
+							<p className='bold-24 px-2 py-4 rounded-md mt-2 sm:mt-6 text-center bg-blue-700/80 text-blue-200 shadow-lg xl:max-w-[720px]'>
 								Diani Beach Podcast is the ultra-shareable, uber-cool online
 								guide to things to do in Diani Beach.
 							</p>
-							<p className='text-md font-semibold p-3 shadow-lg text-blue-900 rounded-md bg-blue-300/50 text-center'>
+							<p className='text-md font-semibold p-3 mb-2 shadow-lg text-blue-900 rounded-md bg-blue-300/50 text-center'>
 								Vincent Embukane Liboso
 							</p>
 						</div>
