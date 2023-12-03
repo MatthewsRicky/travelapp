6959a0ac7b1beeb4bc1bc439d5dddabc216c3dcf
RickyMatthews <matthewsrickypro@gmail.com>
2023-12-03 21:30:19 +0300

    Update BlogCard.tsx

--- components/BlogCard.tsx
+++ components/BlogCard.tsx
@@ -1,7 +1,28 @@
+import Image from "next/image";
 import React from "react";
 
-const BlogCard = () => {
-	return <section></section>;
+const BlogCard = (
+	title: string,
+	description: string,
+	src: string,
+	alt: string,
+	width: number,
+	height: number
+) => {
+	return (
+		<div>
+			<Image
+				src={src}
+				alt={alt}
+				width={width}
+				height={height}
+			/>
+			<div>
+				<h1>{title}</h1>
+				<p>{description}</p>
+			</div>
+		</div>
+	);
 };
 
 export default BlogCard;
