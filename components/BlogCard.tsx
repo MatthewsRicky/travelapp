d907e0f37fd4d62c085277a853d7b615818be7cd
RickyMatthews <matthewsrickypro@gmail.com>
2023-12-04 19:03:12 +0300

    Update BlogCard.tsx

--- components/BlogCard.tsx
+++ components/BlogCard.tsx
@@ -11,17 +11,18 @@ type blogItems = {
 
 const BlogCard = ({ src, alt, title, description }: blogItems) => {
 	return (
-		<div className='grid sm:grid-cols-2 lg:grid-cols-3 gap-8 max-container items-center '>
+		<div className='grid my-10 w-[95%] sm:grid-cols-2 lg:grid-cols-3 gap-8 max-container items-center '>
 			{BLOG_SECTION.map((blog) => (
 				<div
 					key={blog.title}
-					className=''>
+					className='my-6'>
 					<div>
 						<Image
 							src={blog.src}
 							alt={blog.alt}
 							width={1000}
 							height={1000}
+							className='rounded-md mb-6'
 						/>
 						<div>
 							<h1>{blog.title}</h1>
