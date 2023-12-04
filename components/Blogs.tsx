3cf07c5f1da4226f6e9b2ca73db672a442ba7584
RickyMatthews <matthewsrickypro@gmail.com>
2023-12-04 09:25:58 +0300

    Update Blogs.tsx

--- components/Blogs.tsx
+++ components/Blogs.tsx
@@ -4,7 +4,12 @@ import BlogCard from "./BlogCard";
 const Blogs = () => {
 	return (
 		<section>
-			<BlogCard />
+			<BlogCard
+				src='/sarova-stanley-thai-chi-restaurant-9.webp'
+				alt='Sarova Stanely'
+				title='Dine Out at these New Diani Hotspots'
+				description='February is technically the first month, so congratulations: you’ve pretty much survived another nJaanuary (*ask a Kenyan the meaning of this) in Diani. To make it go by quicker, here are some new tasty gourmet spots in town to check out.'
+			/>
 		</section>
 	);
 };
