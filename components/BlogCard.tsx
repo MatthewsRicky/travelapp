d3bbc619389ef9b9493779951ac8cd1b2f9b661a
RickyMatthews <matthewsrickypro@gmail.com>
2023-12-04 09:19:48 +0300

    Update BlogCard.tsx

--- components/BlogCard.tsx
+++ components/BlogCard.tsx
@@ -1,26 +1,24 @@
+import { BLOG_SECTION } from "@/constants";
 import Image from "next/image";
 import React from "react";
 
-const BlogCard = (
-	title: string,
-	description: string,
-	src: string,
-	alt: string,
-	width: number,
-	height: number
-) => {
+const BlogCard = () => {
 	return (
 		<div>
-			<Image
-				src={src}
-				alt={alt}
-				width={width}
-				height={height}
-			/>
-			<div>
-				<h1>{title}</h1>
-				<p>{description}</p>
-			</div>
+			{BLOG_SECTION.map((blog) => (
+				<div>
+					<Image
+						src={src}
+						alt={alt}
+						width={width}
+						height={height}
+					/>
+					<div>
+						<h1>{title}</h1>
+						<p>{description}</p>
+					</div>
+				</div>
+			))}
 		</div>
 	);
 };
