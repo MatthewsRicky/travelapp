5d0e53023cdf37471447d58cbe73a9f0b83d2376
RickyMatthews <matthewsrickypro@gmail.com>
2023-12-06 09:37:24 +0300

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 import Button from "./Button";
 const Hero = () => {
 	return (
-		<section className='relative z-100 max-container video-background padding-container flex flex-col justify-center items-center gap-6 py-10'>
+		<section className='relative z-100 max-container video-background p flex flex-col justify-center items-center gap-6 py-10'>
 			<video
 				autoPlay
 				loop
@@ -16,17 +16,18 @@ const Hero = () => {
 			</video>
 
 			<div className='relative flex flex-col'>
-				<div className='justify-around items-center relative mx-auto bg-blue-100/20 flex z-20 sm:w-[85%] flex-col sm:flex-row gap-4 px-3 py-3 rounded-lg shadow-2xl'>
-					<div className=' flex  flex-col z-20'>
+				<div className='justify-around items-center relative mx-auto flex z-20 sm:w-[85%] flex-col sm:flex-row gap-4 px-3 py-3 rounded-lg shadow-2xl'>
+					<div className=' flex w-[80%] mx-auto z-20'>
 						<Image
 							src='/libosso_profile.webp'
 							alt='Libosso'
-							width={250}
+							width={300}
 							height={250}
 							className='rounded-md drop-shadow-lg shadow-blue-950'
 						/>
 					</div>
 					<div className='flex flex-col gap-4 w-full justify-center mx-auto mt-10 items-center sm:w-[40%]'>
+						<h1>Diani Beach Podcast</h1>
 						<p className='bold-24 px-2 py-4 rounded-lg mt-2 sm:mt-6 text-center bg-blue-700/80 text-blue-200 shadow-lg xl:max-w-[720px]'>
 							Diani Beach Podcast is the ultra-shareable, uber-cool online guide
 							to things to do in Diani Beach.
