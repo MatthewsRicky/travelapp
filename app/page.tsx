2364d386194d76c619d13135a85ad1b1ad1f5edd
RickyMatthews <matthewsrickypro@gmail.com>
2023-12-09 09:40:12 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,7 @@
 import Blogs from "@/components/Blogs";
 
 import Features from "@/components/Features";
-import GetApp from "@/components/GetApp";
-import Guide from "@/components/Guide";
+
 import Hero from "@/components/Hero";
 
 export default function Home() {
@@ -10,9 +9,6 @@ export default function Home() {
 		<>
 			<Hero />
 			<Blogs />
-			<Guide />
-			<Features />
-			<GetApp />
 		</>
 	);
 }
