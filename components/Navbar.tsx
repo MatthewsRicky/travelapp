f846fe1ff1711045e000fc97a9e2964846f0b2c0
RickyMatthews <matthewsrickypro@gmail.com>
2023-12-09 09:41:30 +0300

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -33,7 +33,7 @@ const Navbar = () => {
 	};
 	return (
 		<section className='relative'>
-			<nav className='fixed text-center sm:left-[2.5%] md:left-[10%] lg:left-[17.5%] flex flex-col md:flex-row justify-around items-center max-container padding-container my-6 sm:rounded-full bg-blue-200/60 z-30 py-3 gap-4 w-full sm:w-[95%] md:w-[80%] lg:w-[70%] xl:w-[65%] backdrop-blur-sm'>
+			<nav className='fixed text-center sm:left-[2.5%] md:left-[10%] lg:left-[17.5%] flex flex-col md:flex-row justify-around items-center max-container padding-container my-3 sm:rounded-full bg-blue-200/60 z-30 py-2 gap-4 w-full sm:w-[95%] md:w-[80%] lg:w-[70%] xl:w-[65%] backdrop-blur-sm'>
 				<div className='flex flex-row justify-around w-full'>
 					<Link
 						href='/'
