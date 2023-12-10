148537f98bea276df3d0e9ac65c2ffc35f3254cf
RickyMatthews <matthewsrickypro@gmail.com>
2023-12-10 13:50:24 +0300

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -42,7 +42,7 @@ const Navbar = () => {
 				<motion.div
 					initial={{ y: -100, opacity: 0 }}
 					animate={{ y: [0, 5, 0], opacity: 1 }}
-					transition={{ duration: 0.8 }}
+					transition={{ duration: 0.6 }}
 					className='flex flex-row justify-around w-full'>
 					<Link
 						href='/'
