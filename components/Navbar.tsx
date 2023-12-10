065bcd7bfdb327dd2de2c826dd8d7a6761d57081
RickyMatthews <matthewsrickypro@gmail.com>
2023-12-10 11:27:33 +0300

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -38,7 +38,7 @@ const Navbar = () => {
 				initial={{ y: -100, opacity: 0 }}
 				animate={{ y: 0, opacity: 1 }}
 				transition={{ duration: 0.5 }}
-				className='fixed text-center sm:left-[2.5%] md:left-[10%] lg:left-[17.5%] flex flex-col md:flex-row justify-around items-center max-container padding-container my-3 sm:rounded-full bg-blue-200/60 z-30 py-2 gap-4 w-full sm:w-[95%] md:w-[80%] lg:w-[70%] xl:w-[65%] backdrop-blur-sm'>
+				className='fixed text-center sm:left-[2.5%] md:left-[10%] lg:left-[17.5%] flex flex-col md:flex-row justify-between sm:justify-around items-center max-container padding-container my-3 sm:rounded-full bg-blue-200/60 z-30 py-2 gap-4 w-full sm:w-[95%] md:w-[80%] lg:w-[70%] xl:w-[65%] backdrop-blur-sm'>
 				<motion.div
 					initial={{ y: -100, opacity: 0 }}
 					animate={{ y: [0, 5, 0], opacity: 1 }}
@@ -62,11 +62,6 @@ const Navbar = () => {
 					</ul>
 
 					<div className='flexCenter gap-4 relative right-[-10]'>
-						<button>
-							<Link href='/'>
-								<FaUser />
-							</Link>
-						</button>
 						<button
 							onClick={handleMenuOpen}
 							className={`${
@@ -84,6 +79,11 @@ const Navbar = () => {
 									: "sm:hidden flex relative z-30 duration-300"
 							}`}>
 							<FaAnkh />
+						</button>
+						<button>
+							<Link href='/'>
+								<FaUser />
+							</Link>
 						</button>
 					</div>
 				</motion.div>
