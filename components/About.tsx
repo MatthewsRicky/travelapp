07bbfbb4e0ebe32b3f4465792fa8a3a96775ba38
RickyMatthews <matthewsrickypro@gmail.com>
2023-12-11 17:58:15 +0300

    Create About.tsx

---
+++ components/About.tsx
@@ -0,0 +1,46 @@
+import Image from "next/image";
+import React from "react";
+
+const About = () => {
+	return (
+		<main>
+			<section>
+				<Image />
+				<article>
+					<h1>Meet The Author.</h1>
+					<p>
+						Embukane Vincent Libosso Bengoh started his career at the beginning
+						of 2010 as a blogger, after a one month training at Nation Media
+						Group in August. With a vast grasp in the events and entertainment
+						industry, Kenya Buzz and later on, UP Magazine came calling. From a
+						staff writer, he rose to an online Digital Content Manager. He
+						worked there for a year (2015) before taking on a new challenge at
+						What’s Good Studios.
+					</p>
+					<p>
+						In 2017, he graduated with a Diploma in Communication from Daystar
+						University. At What’s Good Studios he took on the roles on online
+						Managing Editor for their publishing platform, an online radio and
+						TV host. Libosso, better known as The Events Kahuna is not new to
+						the heavy-atmosphere of Nairobi’s Digital airwaves (often
+						crisscrossing into Uganda with Festival coverage and celebrity
+						interviews). The key to his success has been an unsinkable passion
+						for radio, TV, content marketing and creation, music and people.
+					</p>
+					<p>
+						In 2020, alongside co-presenters Rafiiki and Blak Ice, Libosso aka
+						Vinny Puppa Rankin received the prestigious 2020 Sondeka Award for
+						Best Online Radio and Podcast award for their straight reggae and
+						dancehall show, #LazeReggae Invasion Podcast.
+					</p>
+					<button>Contact Me</button>
+				</article>
+			</section>
+			<section>
+				<article></article>
+			</section>
+		</main>
+	);
+};
+
+export default About;
