3d3d9cef74672b78773d987016561dd5900508b0
RickyMatthews <matthewsrickypro@gmail.com>
2023-12-13 21:56:22 +0300

    Update SideBar.tsx

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -1,7 +1,25 @@
+import { SIDEBAR_LINKS } from "@/constants";
+import Link from "next/link";
 import React from "react";
 
 const SideBar = () => {
-	return <div>SideBar</div>;
+	return (
+		<section>
+			<nav>
+				<ul className='hidden justify-center items-center gap-6 sm:flex flex-row'>
+					{SIDEBAR_LINKS.map((link) => (
+						<li className='bg-blue-300/60 px-2 py-0.5 rounded-md text-slate-800 font-medium text-sm md:text-md'>
+							<Link
+								href={link.href}
+								key={link.key}>
+								{link.label}
+							</Link>
+						</li>
+					))}
+				</ul>
+			</nav>
+		</section>
+	);
 };
 
 export default SideBar;
