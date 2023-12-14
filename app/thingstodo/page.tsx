2e35f460b6f95dff1d75a47c0c09184abc6527d6
RickyMatthews <matthewsrickypro@gmail.com>
2023-12-14 23:59:47 +0300

    Create page.tsx

---
+++ app/thingstodo/page.tsx
@@ -0,0 +1,12 @@
+import ThingstoDo from "@/components/ThingstoDo";
+import React from "react";
+
+const page = () => {
+	return (
+		<div>
+			<ThingstoDo />
+		</div>
+	);
+};
+
+export default page;
