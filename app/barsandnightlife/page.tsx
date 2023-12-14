ec2fa136967048cf122a024e083ac002bd4f62c2
RickyMatthews <matthewsrickypro@gmail.com>
2023-12-14 23:57:57 +0300

    Create page.tsx

---
+++ app/barsandnightlife/page.tsx
@@ -0,0 +1,12 @@
+import BarsAndNightlife from "@/components/BarsAndNightlife";
+import React from "react";
+
+const page = () => {
+	return (
+		<div>
+			<BarsAndNightlife />
+		</div>
+	);
+};
+
+export default page;
