867a2d6485a33fac97cd44c5a21ea98a3417ce16
RickyMatthews <matthewsrickypro@gmail.com>
2023-12-14 23:44:00 +0300

    Create page.tsx

---
+++ app/mylivinglist/page.tsx
@@ -0,0 +1,12 @@
+import MyLivingList from "@/components/MyLivingList";
+import React from "react";
+
+const page = () => {
+	return (
+		<div>
+			<MyLivingList />
+		</div>
+	);
+};
+
+export default page;
