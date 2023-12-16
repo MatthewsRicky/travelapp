98706d062c504ac6239b55d915017f0641f8ecb9
RickyMatthews <matthewsrickypro@gmail.com>
2023-12-16 13:02:57 +0300

    Update SideBar.tsx

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -6,14 +6,10 @@ import React from "react";
 const SideBar = () => {
 	return (
 		<section className='relative'>
-			<nav className='fixed text-center sm:left-[2.5%] md:left-[10%] lg:left-[17.5%] flex flex-col md:flex-row justify-between sm:justify-around items-center max-container padding-container my-3 sm:rounded-full bg-blue-200/60 z-30 py-2 gap-4 w-full sm:w-[95%] md:w-[80%] lg:w-[70%] xl:w-[65%] backdrop-blur-sm'>
-				<motion.ul
-					initial={{ x: -100, opacity: 0 }}
-					animate={{ x: 0, opacity: 1 }}
-					transition={{ duration: 0.5 }}
-					className='justify-center items-center gap-6 flex flex-col'>
+			<nav className='fixed text-center left-[2%] top-[27%] flex flex-col justify-between sm:justify-around items-center max-container padding-container my-3 rounded-md  z-30 py-2 gap-4 w-[18%] backdrop-blur-sm'>
+				<ul className='justify-center items-center gap-2 flex flex-col'>
 					{SIDEBAR_LINKS.map((link) => (
-						<li className='bg-blue-300/60 px-2 py-0.5 rounded-md text-slate-800 font-medium text-sm md:text-md'>
+						<li className='bg-slate-300/60 px-2 py-0.5 w-[100%] rounded-md text-slate-800 font-medium text-sm'>
 							<Link
 								href={link.href}
 								key={link.key}>
@@ -21,7 +17,7 @@ const SideBar = () => {
 							</Link>
 						</li>
 					))}
-				</motion.ul>
+				</ul>
 			</nav>
 		</section>
 	);
