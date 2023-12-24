e2406f9f5efa196d482a31152f99a39ac6ef42c7
RickyMatthews <matthewsrickypro@gmail.com>
2023-12-24 23:56:18 +0300

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -76,7 +76,7 @@ const Navbar = () => {
 						</button>
 
 						<Link href='/'>
-							<button className='bg-slate-200/60 flex justify-center text-venter p-2 rounded-full items-center'>
+							<button className='bg-slate-200/60 flex justify-center text-center text-md p-2 rounded-full items-center'>
 								<FaUser />
 							</button>
 						</Link>
