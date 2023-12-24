917f8ad74579f800d57d57a601cddadc841d7f11
RickyMatthews <matthewsrickypro@gmail.com>
2023-12-24 23:58:52 +0300

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -45,7 +45,7 @@ const Navbar = () => {
 					className='flex flex-row justify-around w-full'>
 					<ul className='hidden justify-center items-center gap-6 sm:flex flex-row'>
 						{NAV_LINKS.map((link) => (
-							<li className='bg-slate-300/60 px-2 py-0.5 rounded-md text-slate-900 font-medium text-sm md:text-md'>
+							<li className='bg-slate-300/60 hover:bg-blue-300/60 px-2 py-0.5 rounded-md text-slate-900 font-medium text-sm md:text-md'>
 								<Link
 									href={link.href}
 									key={link.key}>
