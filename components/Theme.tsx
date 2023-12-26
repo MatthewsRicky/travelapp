27798f8a5dc61244cc35efaadace00c9f6acd23f
RickyMatthews <matthewsrickypro@gmail.com>
2023-12-26 15:37:31 +0300

    Update Theme.tsx

--- components/Theme.tsx
+++ components/Theme.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import { FaSun } from "react-icons/fa";
 
 const Theme = () => {
-	return <div>Theme</div>;
+	return (
+		<section>
+			<button>
+				<FaSun />
+			</button>
+		</section>
+	);
 };
 
 export default Theme;
