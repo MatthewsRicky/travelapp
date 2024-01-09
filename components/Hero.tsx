2ef0edcf157347a6d83ccda6ae1defda8095e444
RickyMatthews <matthewsrickypro@gmail.com>
2024-01-09 08:32:15 +0300

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -16,8 +16,8 @@ const Hero = () => {
 			</video>
 
 			<div className='relative flex flex-col'>
-				<div className='justify-around w-[60] items-center relative mx-auto flex z-20 sm:w-[85%] flex-col sm:flex-row gap-4 px-3 py-3 rounded-lg shadow-2xl backdrop-blur-sm'>
-					<div className='hidden sm:flex z-20'>
+				<div className='justify-around w-[60%] items-center relative mx-auto flex z-20 sm:w-[85%] flex-col sm:flex-row gap-4 px-3 py-3 rounded-lg shadow-2xl backdrop-blur-sm'>
+					<div className='hidden sm:flex z-20 rounded-full'>
 						<Image
 							src='/libosso_profile.webp'
 							alt='Libosso'
@@ -27,12 +27,12 @@ const Hero = () => {
 						/>
 					</div>
 					<div className='flex flex-col gap-4 w-full justify-center mx-auto mt-10 items-center sm:w-[40%]'>
-						<h1 className=' backdrop-blur-xs text-slate-100 bg-orange-600/80 text-center uppercase font-bold text-lg p-4 rounded-md'>
+						<h1 className=' backdrop-blur-xs text-slate-100 bg-orange-600/80 text-center uppercase font-bold text-xl md:text-2xl p-4 rounded-md'>
 							Diani Beach Podcast
 						</h1>
 						<p className='bold-24 backdrop-blur-md px-2 py-4 rounded-lg mt-2 sm:mt-6 text-center bg-blue-800/60 text-blue-200 shadow-lg xl:max-w-[720px]'>
-							Diani Beach Podcast is the ultra-shareable, uber-cool online guide
-							to things to do in Diani Beach.
+							" Diani Beach Podcast is the ultra-shareable, uber-cool online
+							guide to things to do in Diani Beach. "
 						</p>
 						<p className='backdrop-blur-md text-md font-semibold p-3 mb-2 shadow-lg text-white rounded-md bg-blue-300/50 text-center'>
 							Vincent Embukane Liboso
