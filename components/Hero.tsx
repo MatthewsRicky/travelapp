6b4c91ac3c1405192485323cefba02e5d861c1ff
RickyMatthews <matthewsrickypro@gmail.com>
2024-01-09 08:28:23 +0300

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -16,14 +16,14 @@ const Hero = () => {
 			</video>
 
 			<div className='relative flex flex-col'>
-				<div className='justify-around items-center relative mx-auto flex z-20 sm:w-[85%] flex-col sm:flex-row gap-4 px-3 py-3 rounded-lg shadow-2xl backdrop-blur-sm'>
+				<div className='justify-around w-[60] items-center relative mx-auto flex z-20 sm:w-[85%] flex-col sm:flex-row gap-4 px-3 py-3 rounded-lg shadow-2xl backdrop-blur-sm'>
 					<div className='hidden sm:flex z-20'>
 						<Image
 							src='/libosso_profile.webp'
 							alt='Libosso'
-							width={250}
-							height={250}
-							className='rounded-md drop-shadow-lg shadow-blue-950'
+							width={125}
+							height={75}
+							className='rounded-full drop-shadow-lg shadow-blue-950'
 						/>
 					</div>
 					<div className='flex flex-col gap-4 w-full justify-center mx-auto mt-10 items-center sm:w-[40%]'>
