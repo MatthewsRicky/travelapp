900062e2124f0c34bec0732eb9471eaf5b6b10b2
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2024-06-18 18:43:36 +0000

    fixing alignment issues still

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -4,10 +4,10 @@ import Image from "next/image";
 import Button from "./Button";
 const Hero = () => {
 	return (
-		<section className='relative z-100 max-container space-y-6 flex flex-col justify-center items-center gap-6 mt-24'>
-			<div className='relative flex flex-col mt-32'>
+		<section className='relative z-100 max-container space-y-6 flex flex-col justify-center items-center gap-6 mt-16'>
+			<div className='relative flex flex-col mt-4'>
 				<div className='justify-around w-[60%] items-center relative mx-auto flex z-20 sm:w-[85%] flex-col sm:flex-col gap-4 px-3 py-3 rounded-lg shadow-2xl backdrop-blur-sm'>
-					<div className='hidden sm:flex z-20 mt-24'>
+					<div className='hidden sm:flex z-20 mt-4'>
 						<Image
 							src='/libosso_profile.webp'
 							alt='Libosso'
