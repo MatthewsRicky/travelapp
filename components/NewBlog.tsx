154a9e4bbb17780f8ffb5ad2a5a0f4691d84ca1a
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2024-06-19 20:01:47 +0000

    building out plan B of the blog section. intergrating react-vertical-timeline-component from the start to try and redesign as I go

--- components/NewBlog.tsx
+++ components/NewBlog.tsx
@@ -1,9 +1,17 @@
 "use client"
-
+import { VerticalTimeline, VerticalTimelineElement } from "react-vertical-timeline-component"
 import { BLOG_SECTION } from "@/constants"
 
+type blogItems = {}
+ 
 const NewBlog = () => {
   return(
-    {BLOG_SECTION.map(()=>())}
+    <VerticalTimeline>
+      <VerticalTimelineElement>
+        {BLOG_SECTION.map(()=>())}
+      </VerticalTimelineElement>
+    </VerticalTimeline>
   )
 }
+
+export default NewBlog
