b027fcfb1e42b093eefbc82f37f106e39f1a2b79
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2024-06-19 20:43:10 +0000

    testing out NewBlog component in the empty directory page.

--- components/Directory.tsx
+++ components/Directory.tsx
@@ -1,7 +1,9 @@
 import React from "react";
-
+import NewBlog from "./NewBlog";
 const Directory = () => {
-	return <div>Directory</div>;
+	return <div>
+		<NewBlog src="" alt="" title="" description="" />
+	</div>;
 };
 
 export default Directory;
