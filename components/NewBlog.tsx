d7c7dbea93473262df7afd268447b16d5586d935
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2024-06-20 15:45:16 +0000

    adding icons for each title. must specify in future push.

--- components/NewBlog.tsx
+++ components/NewBlog.tsx
@@ -2,7 +2,7 @@
 
 import { BLOG_SECTION } from "@/constants"
 import Image from "next/image"
-import {bsFacebook } from "react-icons"
+import { AiFillAccountBook } from "react-icons/ai"
 
 type blogItems = {
   src: string;
@@ -17,7 +17,13 @@ const NewBlog = ({src, alt, title, description}: blogItems) => {
         {BLOG_SECTION.map((blog)=>(
           <div key={blog.title} className="flexCenter flex-col padding-container !px-20">
             <Image src={blog.src} alt={blog.alt} height={500} width={500} className="rounded-sm shadow-lg my-4"/>
-            <h1 className="bold-20 py-3 my-4">{blog.title}</h1>
+            <div className="flexStart gap-4">
+              <span>
+                <AiFillAccountBook />
+              </span>
+              <h1 className="bold-20 py-3 my-4">{blog.title}</h1>
+            </div>
+            
             <p>{blog.description}</p>
           </div>
         ))}
