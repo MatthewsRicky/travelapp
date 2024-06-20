db64ddf246f3c367bc369b858f727cf3d144d603
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2024-06-20 14:58:36 +0000

    testing out react icons dependancy since im developing on code server on an android phone :)

--- components/NewBlog.tsx
+++ components/NewBlog.tsx
@@ -2,7 +2,7 @@
 
 import { BLOG_SECTION } from "@/constants"
 import Image from "next/image"
-import {} from "react-icons"
+import {bsFacebook } from "react-icons"
 
 type blogItems = {
   src: string;
@@ -13,24 +13,15 @@ type blogItems = {
  
 const NewBlog = ({src, alt, title, description}: blogItems) => {
   return(
-
-      
         <div className="flex flex-col items-center justify-center mx-auto w-full mb-6">
         {BLOG_SECTION.map((blog)=>(
-          <div key={blog.title}             className="flexCenter flex-col padding-container !px-20">
-            
+          <div key={blog.title} className="flexCenter flex-col padding-container !px-20">
             <Image src={blog.src} alt={blog.alt} height={500} width={500} className="rounded-sm shadow-lg my-4"/>
             <h1 className="bold-20 py-3 my-4">{blog.title}</h1>
             <p>{blog.description}</p>
-
-            
           </div>
-
-        
         ))}
         </div>
-
-
   )
 }
 
