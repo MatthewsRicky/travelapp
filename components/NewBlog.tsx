8148f15503afd6d5bc466eee816dfd414889c93c
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2024-06-20 16:47:36 +0000

    icon alignment

--- components/NewBlog.tsx
+++ components/NewBlog.tsx
@@ -17,10 +17,10 @@ const NewBlog = ({src, alt, title, description}: blogItems) => {
         {BLOG_SECTION.map((blog)=>(
           <div key={blog.title} className="flexCenter flex-col padding-container !px-20">
             <Image src={blog.src} alt={blog.alt} height={500} width={500} className="rounded-sm shadow-lg my-4"/>
-            <div className="flexStart gap-4">
-              <span>
+            <div className="flexStart gap-4 items-center">
+              <div>
                 <AiFillAccountBook />
-              </span>
+              </div>
               <h1 className="bold-20 py-3 my-4">{blog.title}</h1>
             </div>
             
