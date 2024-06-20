dd2966c22c7aafa77c7dbd7a4c4e5b0b081f28c2
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2024-06-20 17:15:41 +0000

    element rearrangement in the card with title goping to the top.

--- components/NewBlog.tsx
+++ components/NewBlog.tsx
@@ -16,13 +16,14 @@ const NewBlog = ({src, alt, title, description}: blogItems) => {
         <div className="flex flex-col items-center justify-center mx-auto w-full mb-6">
         {BLOG_SECTION.map((blog)=>(
           <div key={blog.title} className="flexCenter flex-col padding-container !px-20">
-            <Image src={blog.src} alt={blog.alt} height={500} width={500} className="rounded-sm shadow-lg my-4"/>
             <div className="flexStart gap-4 items-center">
               <div>
                 <AiFillAccountBook className="w-12 h-12 text-orange-400 shadow-sm rounded-sm p-2 border-1-solid shadow-orange-400" />
               </div>
               <h1 className="bold-20 py-3 my-4">{blog.title}</h1>
             </div>
+            
+            <Image src={blog.src} alt={blog.alt} height={500} width={500} className="rounded-sm shadow-lg my-4"/>
             
             <p>{blog.description}</p>
           </div>
