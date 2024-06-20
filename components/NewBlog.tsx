a4f24bdc29339b1633001a682e6c9da3fe066bbc
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2024-06-20 14:16:58 +0000

    giving the full card mb

--- components/NewBlog.tsx
+++ components/NewBlog.tsx
@@ -15,10 +15,9 @@ const NewBlog = ({src, alt, title, description}: blogItems) => {
   return(
 
       
-        <div className="flex flex-col items-center justify-center mx-auto w-full">
+        <div className="flex flex-col items-center justify-center mx-auto w-full mb-6">
         {BLOG_SECTION.map((blog)=>(
-          <div key={blog.title} 
-            className="flexCenter flex-col padding-container !px-20">
+          <div key={blog.title}             className="flexCenter flex-col padding-container !px-20">
             
             <Image src={blog.src} alt={blog.alt} height={500} width={500} className="rounded-sm shadow-lg my-4"/>
             <h1 className="bold-20 py-3 my-4">{blog.title}</h1>
