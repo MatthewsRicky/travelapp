1620575ef7f3defb74456e30b82e93279d508929
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2024-06-21 20:20:55 +0000

    editing the margins of the elements

--- components/NewBlog.tsx
+++ components/NewBlog.tsx
@@ -13,9 +13,9 @@ type blogItems = {
  
 const NewBlog = ({src, alt, title, description}: blogItems) => {
   return(
-        <div className="flex flex-col items-center justify-center mx-auto w-full mb-6">
+        <div className="flex flex-col items-center justify-center mx-auto w-full my-10">
         {BLOG_SECTION.map((blog)=>(
-          <div key={blog.title} className="flexCenter flex-col padding-container !px-20">
+          <div key={blog.title} className="flexCenter flex-col padding-container !px-20 mb-6">
             <div className="flexStart gap-4 items-center">
               <div>
                 <AiFillAccountBook className="w-12 h-12 text-orange-400 shadow-sm rounded-sm p-2 border-1-solid shadow-orange-400" />
