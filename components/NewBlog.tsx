1bacd70081f74856353c18525f114ef8ea119323
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2024-06-22 19:45:07 +0000

    dark text color

--- components/NewBlog.tsx
+++ components/NewBlog.tsx
@@ -13,10 +13,10 @@ type blogItems = {
  
 const NewBlog = ({src, alt, title, description}: blogItems) => {
   return(
-        <div className="flex flex-col items-center justify-center mx-auto w-full my-20 bg-gradient-to-b from-white to-slate-100 dark:bg-gradient-to-b from-[#000814] to-[#000] dark:text-slate-100">
-          <h1 className="uppercase bold-20 border-b-1 border-solid my-4">blog</h1>
+        <div className="flex flex-col items-center justify-center mx-auto w-full my-20 bg-gradient-to-b from-white to-slate-100 dark:bg-gradient-to-b from-[#000814] to-[#000] dark:text-orange-100">
+          <h1 className="uppercase bold-20 border-b-1 border-solid !md:px-20 my-4">blog</h1>
         {BLOG_SECTION.map((blog)=>(
-          <div key={blog.title} className="flexCenter flex-col md:padding-container !md:px-20 mb-6">
+          <div key={blog.title} className="flexCenter flex-col md:padding-container mb-6">
             <div className="flexStart gap-4 items-center">
               <div>
                 <AiFillAccountBook className="w-8 h-8 text-orange-400 shadow-sm rounded-sm p-2 border-1-solid shadow-orange-400" />
