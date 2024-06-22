13848b5ad7ce701fb2bdb164c57569a727c9a514
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2024-06-22 19:33:47 +0000

    still working on the bg

--- components/NewBlog.tsx
+++ components/NewBlog.tsx
@@ -13,7 +13,7 @@ type blogItems = {
  
 const NewBlog = ({src, alt, title, description}: blogItems) => {
   return(
-        <div className="flex flex-col items-center justify-center mx-auto w-full my-20 bg-gradient-to-b from-white to-slate-100 dark:bg-gradient-to-b from-slate-950 to-black">
+        <div className="flex flex-col items-center justify-center mx-auto w-full my-20 bg-gradient-to-b from-white to-slate-100 dark:bg-gradient-to-b from-[#000080] to-[#000]">
           <h1 className="uppercase bold-20 border-b-1 border-solid my-4">blog</h1>
         {BLOG_SECTION.map((blog)=>(
           <div key={blog.title} className="flexCenter flex-col md:padding-container !md:px-20 mb-6">
