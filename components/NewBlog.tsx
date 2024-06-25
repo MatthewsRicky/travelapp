a8642bf0414dc4d9a1efb7e389e775fda08730e1
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2024-06-25 20:56:28 +0000

    removing the icon for now. Might default to a single one for all titles. Should be more minimalistic that way

--- components/NewBlog.tsx
+++ components/NewBlog.tsx
@@ -14,16 +14,14 @@ type blogItems = {
 
 
  
-const NewBlog = ({src, alt, title, description, icon}: blogItems) => {
+const NewBlog = ({src, alt, title, description}: blogItems) => {
   return(
         <div className="flex flex-col items-center justify-center mx-auto w-full my-20 bg-gradient-to-b from-white to-slate-100 dark:bg-gradient-to-b from-[#000814] to-[#000] dark:text-orange-100">
           <h1 className="uppercase bold-20 border-b-1 border-solid !md:px-20 my-4">blog</h1>
         {BLOG_SECTION.map((blog)=>(
           <div key={blog.title} className="flexCenter flex-col md:padding-container mb-6">
             <div className="flexStart gap-4 items-center">
-              <div className="w-8 h-8 text-orange-400 dark:text-orange-100 dark:shadow-orange-100/70 shadow-sm rounded-sm p-2 border-1-solid shadow-orange-400/70">
-                <span>{blog.icon}</span>
-              </div>
+
               <h2 className="bold-20 py-3 my-4">{blog.title}</h2>
             </div>
             
