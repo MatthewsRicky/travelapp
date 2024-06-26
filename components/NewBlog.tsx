223b234f1adf0436a991092cf09203ff7a1d97b5
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2024-06-26 10:19:16 +0000

    working on the bg and apearance.

--- components/NewBlog.tsx
+++ components/NewBlog.tsx
@@ -17,7 +17,7 @@ type blogItems = {
  
 const NewBlog = ({src, alt, title, description}: blogItems) => {
   return(
-        <div className="flex flex-col items-center justify-center mx-auto w-full my-20 bg-gradient-to-b from-white to-slate-100 dark:bg-gradient-to-b from-slate-900 to-slate-950 dark:text-orange-100">
+        <div className="flex flex-col items-center justify-center mx-auto w-full my-20 bg-gradient-to-b from-white to-slate-100 dark:bg-gradient-to-b from-cyan-900 to-cyan-950 dark:text-orange-100">
           <h1 className="uppercase bold-20 border-b-1 border-solid !md:px-20 my-4">blog</h1>
         {BLOG_SECTION.map((blog)=>(
           <div key={blog.title} className="flexCenter flex-col md:padding-container mb-6">
