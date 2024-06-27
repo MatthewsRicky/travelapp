2a0195e2dd69a67907e5dbacc5e28b3875a8d483
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2024-06-27 16:25:58 +0000

    text

--- components/NewBlog.tsx
+++ components/NewBlog.tsx
@@ -17,7 +17,7 @@ type blogItems = {
  
 const NewBlog = ({src, alt, title, description}: blogItems) => {
   return(
-        <div className="flex flex-col items-center justify-center mx-auto w-full my-20 bg-gradient-to-b from-orange-50/50 to-orange-200/50 text-slate-200 tracking-wide">
+        <div className="flex flex-col items-center justify-center mx-auto w-full my-20 bg-gradient-to-b from-orange-50/50 to-orange-200/50 text-slate-900 tracking-wide">
           <h1 className="uppercase bold-20 border-b-1 border-solid !md:px-20 my-4">blog</h1>
         {BLOG_SECTION.map((blog)=>(
           <div key={blog.title} className="flexCenter flex-col md:padding-container mb-6">
