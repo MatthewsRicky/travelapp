2627d1e9c787f56c8bab75b6446c9cde32907e8a
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2024-06-28 10:38:28 +0000

    Alignment of the columns

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -9,16 +9,16 @@ const Footer = () => {
 		<footer className='flexCenter gap-y-6 mb-0 bg-blue-950 text-slate-100 mb-24'>
 			<section className='flex flex-col py-10 justify-center items-center mx-auto'>
 				<h1>Diani Travel guide</h1>
-				<div className='flex flex-row gap-8 justify-between w-full'>
-					<div className="w-[30%]">
+				<div className='flex flex-row items-center mx-auto w-full'>
+					<div className="flex flex-col justify-center mx-auto item-center w-[30%]">
 						<h1>About Us</h1>
 						<p>Diani Beach Podcast is the ultra-shareable, uber-cool online guide to things to do in Diani Beach. Karibu Diani.</p>
 					</div>
-					<div className="w-[30%]">
+					<div className="flex flex-col justify-center mx-auto items-center w-[30%]">
 						<h1>Find Us</h1>
 						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa eligendi quibusdam magni veniam iste voluptas.</p>
 					</div>
-					<div className="w-[30%]">
+					<div className="flex flex-col justify-center mx-auto items-center w-[30%]">
 						<h1>follow us</h1>
 						<ul className="flex">
 							<li><AiFillFacebook /></li>
