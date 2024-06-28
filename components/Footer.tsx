ea58dc7804bdcfb1b1a4a333b8b0f7aaf1de0e40
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2024-06-28 08:20:51 +0000

    filler content to help visualize the styling.

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,19 +2,30 @@ import { FOOTER_CONTACT_INFO, FOOTER_LINKS, SOCIALS } from "@/constants";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import { AiFillFacebook, AiFillInstagram, AiFillTwitterCircle } from "react-icons/ai";
 
 const Footer = () => {
 	return (
 		<footer className='flexCenter gap-y-6 mb-0 bg-blue-950 text-slate-100 mb-24'>
 			<section className='flex flex-col py-10 justify-center items-center mx-auto'>
 				<h1>Diani Travel guide</h1>
-				<div className='flex gap-8 justify-between w-full'>
+				<div className='flex flex-row gap-8 justify-between w-full'>
 					<div>
 						<h1>About Us</h1>
 						<p>Diani Beach Podcast is the ultra-shareable, uber-cool online guide to things to do in Diani Beach. Karibu Diani.</p>
 					</div>
-					<div>Find Us</div>
-					<div>Follow Us</div>
+					<div>
+						<h1>Find Us</h1>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa eligendi quibusdam magni veniam iste voluptas.</p>
+					</div>
+					<div>
+						<h1>follow us</h1>
+						<ul className="flex">
+							<li><AiFillFacebook /></li>
+							<li><AiFillInstagram /></li>
+							<li><AiFillTwitterCircle /></li>
+						</ul>
+					</div>
 				</div>
 			</section>
 		</footer>
