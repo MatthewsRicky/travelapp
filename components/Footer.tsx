02b19617f561ca4f0cef9624d39b92cd2ca3b946
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2024-06-28 08:06:18 +0000

    about us text

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,11 +5,14 @@ import React from "react";
 
 const Footer = () => {
 	return (
-		<footer className='flexCenter gap-y-6 mb-0 bg-blue-950 mb-24'>
+		<footer className='flexCenter gap-y-6 mb-0 bg-blue-950 text-slate-100 mb-24'>
 			<section className='flex flex-col py-10 justify-center items-center mx-auto'>
 				<h1>Diani Travel guide</h1>
 				<div className='flex gap-8 justify-between w-full'>
-					<div>About Us</div>
+					<div>
+						<h1>About Us</h1>
+						<p>Diani Beach Podcast is the ultra-shareable, uber-cool online guide to things to do in Diani Beach. Karibu Diani.</p>
+					</div>
 					<div>Find Us</div>
 					<div>Follow Us</div>
 				</div>
