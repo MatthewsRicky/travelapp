c6d2b000f237692ca023f99240ab924cd13fd223
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2024-06-29 11:20:34 +0000

    Bg, padding, image rounding

--- components/NewBlog.tsx
+++ components/NewBlog.tsx
@@ -16,9 +16,9 @@ type blogItems = {
  
 const NewBlog = ({src, alt, title, description}: blogItems) => {
   return(
-        <div className="flex flex-col items-center justify-center mx-auto w-full bg-gradient-to-b from-orange-50/50 to-orange-200/50 text-slate-900 tracking-wide">
+        <div className="flex flex-col items-center justify-center mx-auto w-full bg-white text-slate-900 tracking-wide">
           <div className="my-20">
-            <h1 className="flex items-center mx-auto justify-center w-full uppercase bold-20 border-b-1 border-solid !md:px-20 my-4">blog</h1>
+            <h1 className="flex items-center mx-auto justify-center w-full uppercase bold-20 border-b-1 border-solid my-4">blog</h1>
               {BLOG_SECTION.map((blog)=>(
                 <div key={blog.title} className="flexCenter flex-col md:padding-container mb-6">
                   <div className="flexStart gap-x-4 items-center">
@@ -28,7 +28,7 @@ const NewBlog = ({src, alt, title, description}: blogItems) => {
                     <h2 className="bold-20 py-3 my-4">{blog.title}</h2>
                   </div>
                   
-                  <Image src={blog.src} alt={blog.alt} height={500} width={500} className="rounded-sm shadow-lg w-full my-4"/>
+                  <Image src={blog.src} alt={blog.alt} height={500} width={500} className="rounded-md shadow-lg w-full my-4"/>
                   
                   <p className="tracking-wide px-12 text-md font-normal">{blog.description}</p>
                 </div>
