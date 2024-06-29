40d0d7119799ddf8896e29f800a555054e97c956
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2024-06-29 14:10:38 +0000

    Reorganizing the page elements

--- components/NewBlog.tsx
+++ components/NewBlog.tsx
@@ -20,16 +20,20 @@ const NewBlog = ({src, alt, title, description}: blogItems) => {
           <div className="my-20">
             <h1 className="flex items-center justify-center w-full uppercase bold-20 border-b-1  border-solid my-4">blog</h1>
               {BLOG_SECTION.map((blog)=>(
-                <div key={blog.title} className="flexCenter flex-col md:padding-container mb-6">
-                  <div className="flexStart gap-x-4 items-center">
-                    
-                      <AiFillStar className="w-6 h-5 text-slate-900 drop-shadow-lg drop-shadow-sky-800 p-6 border-neutral-700-solid" />
+                <div key={blog.title} className="flexCenter md:padding-container mb-6">
                   
-                    <h2 className="bold-20 py-3 my-4">{blog.title}</h2>
-                  </div>
+                  
+                
                   
                   <Image src={blog.src} alt={blog.alt} height={500} width={500} className="rounded-md shadow-lg w-full my-4"/>
+                  <div className="flex gap-2">
+                 <span>
+                 <AiFillStar className="w-6 h-5 text-slate-900 drop-shadow-lg drop-shadow-sky-800 p-6 border-neutral-700-solid" />
+                  </span> 
                   
+                  <h2 className="bold-20 py-3 my-4">{blog.title}</h2>
+
+                    </div>
                   <p className="tracking-wide px-12 text-md font-normal">{blog.description}</p>
                 </div>
               ))}
