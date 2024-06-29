63052d516f4609b4c0d34d212c1751619f9dedf2
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2024-06-29 11:06:06 +0000

    spacing

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -11,11 +11,11 @@ const Footer = () => {
 				<h1>Diani Travel guide</h1>
 				<div className='flex flex-row items-center mx-auto w-full'>
 					<div className="flex flex-col justify-center mx-auto items-center p-4 w-[30%]">
-						<h1>About Us</h1>
+						<h1 className="flex flex-col gap-y-3 m-4 p-4">About Us</h1>
 						<p>Diani Beach Podcast is the ultra-shareable, uber-cool online guide to things to do in Diani Beach. Karibu Diani.</p>
 					</div>
 					<div className="flex flex-col justify-center mx-auto items-center w-[30%]">
-						<h1>Find Us</h1>
+						<h1 className="flex flex-col gap-y-3 m-4 p-4">Find Us</h1>
 						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa eligendi quibusdam magni veniam iste voluptas.</p>
 					</div>
 					<div className="flex flex-col justify-center mx-auto items-center w-[30%]">
