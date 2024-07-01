18b923fa4c8613bec34c1f2f22fd101382721a28
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2024-07-01 17:09:19 +0000

    font styling

--- components/NewBlog.tsx
+++ components/NewBlog.tsx
@@ -9,10 +9,7 @@ type blogItems = {
   title: string;
   description: string;
 
-}
-
-
- 
+} 
 const NewBlog = ({src, alt, title, description}: blogItems) => {
   return(
         <div className="flex flex-col items-center justify-center mx-auto w-full bg-white text-slate-900 tracking-wide">
@@ -23,8 +20,8 @@ const NewBlog = ({src, alt, title, description}: blogItems) => {
                   <Image src={blog.src} alt={blog.alt} height={500} width={500} className="rounded-md shadow-lg w-full max-w-[50%] my-4"/>
                   <div className="flex flex-col md:flex-row gap-2">
                     <div className="flex flex-col items-center gap-3"> 
-                      <h2 className="medium-48 mx-auto flexCenter text-center px-2 py-3 my-4">{blog.title}</h2>
-                      <p className="tracking-wide px-12 medium-20 font-normal">{blog.description}</p>
+                      <h2 className="medium-48 mx-auto flexCenter uppercase text-center px-2 py-3 my-4">{blog.title}</h2>
+                      <p className="tracking-wide px-12 medium-20">{blog.description}</p>
                     </div>
                   </div>
                 </div>
