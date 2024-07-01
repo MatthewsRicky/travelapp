9611fca0aec6472a1adbaa21c30cdf8ebb9c9171
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2024-07-01 17:19:50 +0000

    card shadow

--- components/NewBlog.tsx
+++ components/NewBlog.tsx
@@ -14,9 +14,9 @@ const NewBlog = ({src, alt, title, description}: blogItems) => {
   return(
         <div className="flex flex-col items-center justify-center mx-auto w-full bg-white text-slate-900 tracking-wide">
           <div className="my-20">
-            <h1 className="flex items-center justify-center w-full uppercase bold-20 border-b-1  border-solid my-4">blog</h1>
+            <h1 className="flex items-center justify-center w-full uppercase bold-20 border-b-1 border-solid my-4">blog</h1>
               {BLOG_SECTION.map((blog)=>(
-                <div key={blog.title} className="flexCenter md:padding-container mb-6">
+                <div key={blog.title} className="flexCenter md:padding-container shadow-md shadow-slate-900 p-4 mb-6">
                   <Image src={blog.src} alt={blog.alt} height={500} width={500} className="rounded-md shadow-lg w-full max-w-[50%] my-4"/>
                   <div className="flex flex-col md:flex-row gap-2">
                     <div className="flex flex-col items-center gap-3"> 
