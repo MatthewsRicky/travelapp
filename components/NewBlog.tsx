90c377ba2be7676000e123bc9edcb9910bfb802a
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2024-07-01 17:24:15 +0000

    title shadow and general gap in the card component elements

--- components/NewBlog.tsx
+++ components/NewBlog.tsx
@@ -16,11 +16,11 @@ const NewBlog = ({src, alt, title, description}: blogItems) => {
           <div className="my-20">
             <h1 className="flex items-center justify-center w-full uppercase bold-20 border-b-1 border-solid my-4">blog</h1>
               {BLOG_SECTION.map((blog)=>(
-                <div key={blog.title} className="flexCenter md:padding-container shadow-md shadow-slate-900 p-4 mb-6">
+                <div key={blog.title} className="flexCenter md:padding-container shadow-md shadow-slate-900 gap-4 p-4 mb-6">
                   <Image src={blog.src} alt={blog.alt} height={500} width={500} className="rounded-md shadow-lg w-full max-w-[50%] my-4"/>
                   <div className="flex flex-col md:flex-row gap-2">
                     <div className="flex flex-col items-center gap-3"> 
-                      <h2 className="medium-48 mx-auto flexCenter uppercase text-center px-2 py-3 my-4">{blog.title}</h2>
+                      <h2 className="medium-48 mx-auto flexCenter uppercase shadow-xs shadow-slate-900/50 text-center px-2 py-3 my-4">{blog.title}</h2>
                       <p className="tracking-wide px-12 medium-20">{blog.description}</p>
                     </div>
                   </div>
