7fb8abe17702ceb6ad6774af9a9d459cce902a11
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2024-07-04 14:46:46 +0000

    creation of auth form for credential input

--- modals/AuthModal.tsx
+++ modals/AuthModal.tsx
@@ -2,7 +2,19 @@ import React from 'react'
 
 const AuthModal = () => {
   return (
-    <div>AuthModal</div>
+    <div>
+      import { authenticate } from '@/app/lib/actions'
+ 
+
+
+     <form action={authenticate}>
+       <input type="email" name="email" placeholder="Email" required />
+       <input type="password" name="password" placeholder="Password" required />
+       <button type="submit">Login</button>
+     </form>
+   )
+ }
+    </div>
   )
 }
 
