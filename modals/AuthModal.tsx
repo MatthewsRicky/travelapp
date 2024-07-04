f1eb44fe161ab9d8a1e16ca2239546c236b7a927
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2024-07-04 18:20:43 +0000

    building out the form

--- modals/AuthModal.tsx
+++ modals/AuthModal.tsx
@@ -1,19 +1,17 @@
 import React from 'react'
+import { authenticate } from '@/lib/action'
+
 
 const AuthModal = () => {
   return (
     <div>
-      import { authenticate } from '@/app/lib/actions'
- 
-
-
      <form action={authenticate}>
        <input type="email" name="email" placeholder="Email" required />
        <input type="password" name="password" placeholder="Password" required />
        <button type="submit">Login</button>
      </form>
-   )
- }
+
+ 
     </div>
   )
 }
