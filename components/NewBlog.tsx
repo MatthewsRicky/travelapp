06f73422b6ecddcfed48f86059d71e27e9837795
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2024-07-08 07:35:51 +0000

    factoring out the link that was causing issues to it's own server component

--- components/NewBlog.tsx
+++ components/NewBlog.tsx
@@ -22,11 +22,7 @@ const NewBlog = ({src, alt, title, description}: blogItems) => {
                     <div className="flex flex-col items-center gap-3"> 
                       <h2 className="medium-48 mx-auto flexCenter uppercase gap-2 text-center px-2 py-3 my-4">{blog.title}</h2>
                       <p className="tracking-wide mx-2 py-4 px-12 medium-20 shadow-sm shadow-orange-900/30">{blog.description}
-             
-
-											
-											<p className='text-sky-500 hover:underline'>...Continue Reading</p>
- 
+                     
                     {/* this link causes a three error warning on the page. sort it */}
                       </p>
                     </div>
